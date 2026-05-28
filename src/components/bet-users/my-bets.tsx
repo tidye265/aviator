1b8b8c76b302b30474850e920d1e86f049bb7f18
tidye265 <moskonx@gmail.com>
2026-05-28 15:08:47 +0200

    Update my-bets.tsx

--- src/components/bet-users/my-bets.tsx
+++ src/components/bet-users/my-bets.tsx
@@ -11,9 +11,9 @@ const MyBets = () => {
             <div className="legend">
                 <div className="date">Date</div>
                 <div className="bet-100">
-                    <span className="bet">Bet, INR</span>
+                    <span className="bet">Bet, MWK</span>
                     <span>X</span>
-                    <span className="cash-out"> Cash out, INR </span>
+                    <span className="cash-out"> Cash out, MWK </span>
                 </div>
                 <div className="tools"></div>
             </div>
