11ba7727a6988121682eb25a19e0b602b0d28158
tidye265 <moskonx@gmail.com>
2026-05-29 15:23:19 +0200

    Update history.tsx

--- src/components/Main/history.tsx
+++ src/components/Main/history.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import { useCrashContext } from "./context";
+import { useCrashContext } from "./context";
 import Context from "../../context";
 
 export default function History() {
